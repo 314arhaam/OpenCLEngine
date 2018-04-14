54a83c64c81c115f3e3210e41478dd7feaa21b9e
Parham <prmbas@gmail.com>
2018-04-14 22:20:13 +0430

    comments added

--- main.cpp
+++ main.cpp
@@ -16,12 +16,19 @@
 #include "Host.h"
 
 int main(int argc, const char * argv[]) {
+    // input data
+    // they'll be from a real source in a real problem
     float a[NUM_VALUES] = {1};
     float b[NUM_VALUES] = {2};
     float c[NUM_VALUES] = {};
+    
     clock_t begin = clock();
+    // create GPU queue
     dispatch_queue_t __queue__ = createQueue();
+    // sending data to host
+    // data will be proceed through GPU memory within host
     host(__queue__, a, b, c);
+    // release queue after using
     dispatch_release(__queue__);
     clock_t end = clock();
     float elapsed_secs1 = float(end - begin) / CLOCKS_PER_SEC;
